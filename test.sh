4552da24f132002e3d0f96302f2ef17cb8335ae9
Doctor Andonuts <gowingjm@gmail.com>
2015-03-11 20:11:27 -0400

    Fix tests

--- test.sh
+++ test.sh
@@ -6,7 +6,7 @@ normal=`tput sgr0`
 
 function reset {
 	echo ''
-	cp data.json.bak data.json
+	cp ~/chain/test.json ~/.chain.json
 }
 
 function printHeader() {	
@@ -14,61 +14,66 @@ function printHeader() {
 }
 
 
+cp ~/.chain.json ~/.chain.json.bak
+
 # Print
 reset
 printHeader "Print"
-./chain.py
+chain
 
 
 # Delete
 reset
 printHeader "Delete 1"
-./chain.py 1 delete
-./chain.py
+chain 1 delete
+chain
 
 reset
 printHeader "Delete 2"
-./chain.py 2 delete
-./chain.py
+chain 2 delete
+chain
 
 reset
 printHeader "Delete 3"
-./chain.py 3 delete
-./chain.py
+chain 3 delete
+chain
 
 
 # Add
 reset
 printHeader "Add 3"
-./chain.py add Test 5 6
-./chain.py
+chain add Test 5 6
+chain
 
 
 # Mod
 reset
 printHeader "Mod 1"
-./chain.py 1 mod "Changed 1" 5 6
-./chain.py
+chain 1 mod "Changed 1" 5 6
+chain
 
 reset
 printHeader "Mod 2"
-./chain.py 2 mod MORE 2 2
-./chain.py
+chain 2 mod MORE 2 2
+chain
 
 
 # Done
 reset
 printHeader "Done 1"
-./chain.py 1 done
-./chain.py
+chain 1 done
+chain
 
 reset
 printHeader "Done 1"
-./chain.py 1 done 2015-03-08
-./chain.py
+chain 1 done 2015-03-08
+chain
 
 reset
 printHeader "Done 2"
-./chain.py 2 done
-./chain.py
+chain 2 done
+chain
 
+
+
+cp ~/.chain.json.bak ~/.chain.json
